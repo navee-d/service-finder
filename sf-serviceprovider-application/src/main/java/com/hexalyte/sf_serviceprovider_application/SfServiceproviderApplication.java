2ce923a3ccb3caea23560bd9ff485234ec521340
Ravishka Bahshitha <ravishka@hexalyte.com>
2025-02-26 23:17:42 +0530

    Module initialization

---
+++ sf-serviceprovider-application/src/main/java/com/hexalyte/sf_serviceprovider_application/SfServiceproviderApplication.java
@@ -0,0 +1,13 @@
+package com.hexalyte.sf_serviceprovider_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfServiceproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfServiceproviderApplication.class, args);
+    }
+
+}

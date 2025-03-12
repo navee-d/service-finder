3424789d6bffbadd318a0bc627f125eeb0a10c79
Ramin Senanayake <ramin@hexalyte.com>
2025-03-13 00:12:59 +0530

    Gallery module and minio docker container initialization

---
+++ sf-gallery-application/src/main/java/com/hexalyte/sf_gallery_application/GalleryApplication.java
@@ -0,0 +1,13 @@
+package com.hexalyte.sf_gallery_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryApplication.class, args);
+	}
+
+}

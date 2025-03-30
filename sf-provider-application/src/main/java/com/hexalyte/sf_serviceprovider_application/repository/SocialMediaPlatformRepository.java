a6a0e967589a0b3754b268ab2d1b380ad5737fa3
Hexalyte Technology <info@hexalyte.com>
2025-03-30 22:11:38 +0530

    Revert "Service Provider registration"

--- sf-provider-application/src/main/java/com/hexalyte/sf_serviceprovider_application/repository/SocialMediaPlatformRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hexalyte.sf_serviceprovider_application.repository;
-
-import com.hexalyte.sf_serviceprovider_application.model.SocialMediaPlatform;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocialMediaPlatformRepository extends JpaRepository<SocialMediaPlatform, Long> {
-
-}
--- sf-service-application/src/main/java/com/hexalyte/sf_service_application/repository/SolutionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hexalyte.sf_service_application.repository;
-
-import com.hexalyte.sf_service_application.model.Solution;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SolutionRepository extends JpaRepository<Solution,Long> {
-}

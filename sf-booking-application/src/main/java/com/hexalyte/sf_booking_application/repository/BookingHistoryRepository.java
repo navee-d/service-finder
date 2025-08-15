a475db9d4b24423400aecbaf504f41d0e7a07085
Ramin Senanayake <ramin@hexalyte.com>
2025-08-15 18:37:01 +0530

    Update: deleted Booking History model and repository

--- sf-booking-application/src/main/java/com/hexalyte/sf_booking_application/repository/BookingHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hexalyte.sf_booking_application.repository;
-
-import com.hexalyte.sf_booking_application.model.BookingHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingHistoryRepository extends JpaRepository<BookingHistory,Long> {
-}

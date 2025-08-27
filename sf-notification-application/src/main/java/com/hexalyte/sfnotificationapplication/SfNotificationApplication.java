3a494f4d21086885520a5d1fa7ca000d47ff50de
Ramin Senanayake <ramin@hexalyte.com>
2025-08-27 21:48:50 +0530

    Email sending of Notification Feature is complete

---
+++ sf-notification-application/src/main/java/com/hexalyte/sfnotificationapplication/SfNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.hexalyte.sfnotificationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfNotificationApplication.class, args);
+    }
+
+}
